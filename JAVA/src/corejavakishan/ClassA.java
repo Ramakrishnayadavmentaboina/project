76240db8fed462b5fd7f6be5050e8317f2ecff3b
DELL <DELL@DESKTOP-TLM9OQ4>
2022-05-02 16:06:43 +0530

    first commit

---
+++ JAVA/src/corejavakishan/ClassA.java
@@ -0,0 +1,12 @@
+package corejavakishan;
+
+public class ClassA 
+{
+	public static void main(String[] args) {
+		
+		System.out.println("meth1 called");
+	}
+}
+
+
+
---
+++ JAVA/src/corejavakishan/While.java
@@ -0,0 +1,33 @@
+package corejavakishan;
+
+public class While 
+{
+	void meth1(int b)
+	{
+		int a=10;
+		while(a<=20)
+		{
+			if(a==15)
+			{
+				while(b<=30)
+				{
+					if(b==25)
+					{
+						break;
+					}
+					System.out.println("b value:"+b);
+					b++;
+					
+					}
+				}
+			System.out.println("a value:"+a);
+			a++;
+			}
+		}
+	public static void main(String[] args) 
+		// TODO Auto-generated method stub
+		{
+         new While().meth1(16);
+	}
+
+}
